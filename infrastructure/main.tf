951ae8686d4df52323511bb3d121952191a84669
Alexej Disterhoft <alexej@disterhoft.de>
2024-08-06 22:43:53 +0200

    feat: Add private network and subnet for HCloud servers

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -12,19 +12,40 @@ resource "hcloud_ssh_key" "me" {
   public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDCBMdl3Z8zgAUVF5AeY8hz3vcnkrJ+dPh3yYIKGm2ZO"
 }
 
+resource "hcloud_network" "private" {
+  name     = "private"
+  ip_range = "10.0.1.0/16"
+}
+
+resource "hcloud_network_subnet" "cluster" {
+  network_id   = hcloud_network.private.id
+  type         = "cloud"
+  network_zone = "eu-central"
+  ip_range     = "10.0.1.0/24"
+}
+
 resource "hcloud_server" "node" {
   count = 3
 
   name        = "node-${count.index}"
   server_type = "cx42"
   image       = "ubuntu-22.04"
-
   location = "nbg1"
 
   ssh_keys = [
     hcloud_ssh_key.me.id,
     hcloud_ssh_key.rke.id
   ]
+
+  network {
+    network_id = hcloud_network.private.id
+    ip = cidrhost(hcloud_network_subnet.cluster.ip_range, count.index + 10)
+  }
+
+  public_net {
+    ipv4_enabled = true
+    ipv6_enabled = false
+  }
 
   user_data = <<-EOF
     #cloud-config
@@ -45,6 +66,8 @@ resource "hcloud_server" "node" {
       - curl -fsSL https://get.docker.com | sh
       - reboot
     EOF
+
+  depends_on = [ hcloud_network_subnet.cluster ]
 }
 
 output "rke_cluster_yml" {
@@ -54,6 +77,7 @@ output "rke_cluster_yml" {
     nodes : [
       for node in hcloud_server.node : {
         address : node.ipv4_address
+        internal_address : tolist(node.network)[0].ip
         role : ["controlplane", "worker", "etcd"]
         user : "root"
         ssh_key : tls_private_key.ssh.private_key_pem
